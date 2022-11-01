f9eb3d8a23483d91f90f0edb49c343b25a859357
yoshinashi <yoshinashi4410@gmail.com>
2022-11-01 21:10:32 +0900

    chat機能の変更課程

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,7 +7,7 @@
         <link href="https:fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/view.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="{{ asset('js/comment.js') }}"></script>
     </head>
     
