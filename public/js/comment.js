84f01f704427e19fb2c7e3745c17795de63ff2ba
yoshinashi <yoshinashi4410@gmail.com>
2022-11-01 21:25:28 +0900

    chat機能の変更now

--- public/js/comment.js
+++ public/js/comment.js
@@ -4,7 +4,7 @@ $(function() {
 
 function get_data() {
     $.ajax({
-        url: "result/ajax/",
+        url: "result/ajax",
         dataType: "json",
         success: data => {
             $("#comment-data")
