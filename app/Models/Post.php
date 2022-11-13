7283e8f2c61dc65ac59065b478fad2d3c69d7110
yoshinashi <yoshinashi4410@gmail.com>
2022-11-13 17:51:25 +0900

    Googleアカウント前

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -41,6 +41,7 @@ public function getByLimit(int $limit_count = 16)
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
+
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 //外部にあるPostControllerクラスをインポート。
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/indexes', [PostController::class, 'index']);     
 Route::get('/posts/{post}', [PostController::class ,'show']);
-Route::get('/hosts', [PostController::class, 'host']); 
+Route::get('/hosts', [UserController::class, 'host']); 
 Route::get('/creates', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
@@ -30,8 +31,8 @@ Route::delete('/posts/{post}', [PostController::class,'delete']);
 
 
 Route::get('/members', [ProfileController::class, 'member']); 
-Route::get('/users', [ProfileController::class, 'user']);
-Route::get('/accounts/{user}', [ProfileController::class ,'account']);
+Route::get('/users', [UserController::class, 'user']);
+Route::get('/accounts/{user}', [UserController::class ,'account']);
 Route::get('/profiles', [ProfileController::class, 'profile']);
 Route::post('/keeps', [ProfileController::class, 'keep']);
 Route::get('/remakes', [ProfileController::class, 'remake']);
@@ -47,9 +48,9 @@ Route::put('/users/{active}', [ProfileController::class, 'repost']);
 Route::delete('/users/{active}', [ProfileController::class,'delete']);
 
 
-Route::get('/chats', [HomeController::class, 'chat'])->name('chat'); 
+Route::get('/chats/{user}', [HomeController::class, 'chat'])->name('chat'); 
 Route::post('/add', [HomeController::class,'add'])->name('add');
-Route::get('/result/ajax', [HomeController::class,'getData']);
+Route::get('/result/ajax/{user}', [HomeController::class,'getData']);
 
 Route::get('/dashboard',function () {
     return view('dashboard');
