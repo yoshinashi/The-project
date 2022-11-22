3a288588a3676b5ba1cdb71497d50335e9c7a613
yoshinashi <yoshinashi4410@gmail.com>
2022-11-22 15:28:55 +0900

    レジスターの修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,12 +16,12 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline index-account">会員登録</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline index-account">[会員登録]</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">[ログイン]</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">[新規会員登録]</a>
                         @endif
                     @endauth
                 </div>
